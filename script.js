9b0ed93e063c4021a37d48611d4fa037d0a641f8
Allyson1602 <aathyrson@yahoo.com.br>
2021-11-21 19:25:33 -0300

    Melhorias no portifolio

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-    $('.projetos-close').hover(function(){
-        // $('.grupo-projetos-md').toggle('slow');
-        // $(this).toggle('slow');
-
-        // $('.btn-projetos-md i').toggleClass('efeito-projetos-md');
-    });
 
     $('.icone-mais-info').on('click', function(){
         $(this).css('color', 'white');
@@ -17,16 +11,16 @@ $(document).ready(function(){
         acc[i].addEventListener('click', function(){
             this.classList.toggle('active');
 
-            var panel = this.nextElementSibling;
-            if(panel.style.display === 'block'){
-                panel.style.display = 'none';
+            var painel = this.nextElementSibling;
+            if(painel.style.display === 'block'){
+                painel.style.display = 'none';
             }else{
-                panel.style.display = 'block';
+                painel.style.display = 'block';
             }
-            if(panel.style.maxHeight){
-                panel.style.maxHeight = null;
+            if(painel.style.maxHeight){
+                painel.style.maxHeight = null;
             }else{
-                panel.style.maxHeight = panel.scrollHeight + 'px';
+                painel.style.maxHeight = painel.scrollHeight + 'px';
             }
         });
     }
